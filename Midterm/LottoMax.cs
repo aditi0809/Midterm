e44f5fb67356ce4b3dc1bf01511c31a8ff9b2509
AditiKapoor56 <a502@my.centennialcollege.ca>
2020-07-17 20:49:47 -0400

    added LottoMax class

---
+++ Midterm/LottoMax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    /**
+    * <summary>
+    * This class is a subclass of the LottoGame abstract superclass
+    * </summary>
+    * 
+    * @class LottoMax
+    */
+    public class LottoMax : LottoGame, IGenerateLottoNumbers
+    {
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public LottoMax()
+            : base(7, 49)
+        {
+
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
+        public void GenerateLottoNumbers()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                this.PickElements();
+                Console.Write($"Ticket   {i}= ");
+                Console.WriteLine(ToString());
+            }
+        }
+    }
+}
